773f556e39c8315baf9c810ecd280b19efb30193
omarHussamm <omarmero2000@gmail.com>
2023-04-13 00:05:23 +0200

    Initial Commit

---
+++ src/main/java/com/example/Costco/CostcoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Costco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Costco/CostcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Costco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
