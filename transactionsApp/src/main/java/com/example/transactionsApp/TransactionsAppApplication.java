956f4fe7f9faf529d8834f8bb261cae6881c78d7
omarHussamm <omarmero2000@gmail.com>
2023-04-13 00:25:46 +0200

    add transactions and controller services

---
+++ transactionsApp/src/main/java/com/example/transactionsApp/TransactionsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsAppApplication.class, args);
+	}
+
+}
