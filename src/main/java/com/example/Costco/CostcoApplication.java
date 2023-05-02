1929c2b051f74f93e6409197fcc51c50f5bee4bd
abdelrahmanhany1 <abdel-rahman.farouk@student.guc.edu.eg>
2023-05-02 18:27:08 +0300

    Update CostcoApplication.java

--- src/main/java/com/example/Costco/CostcoApplication.java
+++ src/main/java/com/example/Costco/CostcoApplication.java
@@ -2,12 +2,21 @@ package com.example.Costco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CostcoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CostcoApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		System.out.println("hi");
+		return "Disha";
 	}
 
 }
